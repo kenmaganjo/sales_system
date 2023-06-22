d0d85a1d19b2cd7554ea88d1d46afe5d7065d9c0
kenmaganjo <maganjokennedy@gmail.com>
2023-06-22 11:03:38 +0300

    submission of form data to products and sales tables in the DB

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from pgfunc import fetch_data
+from flask import Flask, render_template, request, redirect
+from pgfunc import fetch_data, insert_products, insert_sales
 
 # Create an object called app
 # __name__ is used to tell Flask where to access HTML Files
@@ -22,6 +22,28 @@ def products():
 def sales():
    sales = fetch_data("sales")
    return render_template('sales.html', sales=sales)
+
+@app.route('/addproducts', methods=["POST","GET"])
+def addproducts():
+   if request.method == "POST":
+      name=request.form["name"]
+      buying_price=request.form["buying_price"]
+      selling_price=request.form["selling_price"]
+      stock_quantity=request.form["stock_quantity"]
+      product=(name,buying_price,selling_price,stock_quantity)
+      insert_products(product)
+      return redirect("/products")
+   
+@app.route('/addsales', methods=["POST","GET"])
+def addsales():
+   if request.method == "POST":
+      pid=request.form["pid"]
+      quantity=request.form["quantity"]
+      created_at=request.form["created_at"]
+      sale=(pid,quantity,created_at)
+      insert_sales(sale)
+      return redirect("/sales")
+   
 
 app.run()
 
